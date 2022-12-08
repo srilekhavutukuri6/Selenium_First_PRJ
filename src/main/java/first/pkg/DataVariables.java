0a9def0d9182b842a0920d2fa8021603a68ed26d
srilekhavutukuri6 <srilekhavuturi6@gmail.com>
2022-12-08 16:24:13 -0600

    changes made

---
+++ src/main/java/first/pkg/DataVariables.java
@@ -0,0 +1,15 @@
+package first.pkg;
+
+public class DataVariables {
+public static void main(String[] args) {
+
+
+    byte a = 127;
+    short b = 150;
+    int c = 2000;
+    float d = 300.50f;
+    double e = 1000.50;
+System.out.println(a+" "+b+" "+c+" "+d+" "+e);
+}
+
+}
